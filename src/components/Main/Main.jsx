2a1e2fe7b7e9f4b160983d8463285fe3078c7096
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-06-07 07:42:20 +0500

    main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ReactTyped} from "react-typed";
 import './Main.css'
 import rasm from '../../assets/img/rasm1.png'
 import { PiHandbagSimpleFill } from "react-icons/pi";
@@ -7,18 +8,39 @@ import { Link } from 'react-scroll';
 const Main = () => {
     return (
         <div>
-             <section id="main">
+            <section id="main">
                 <div className="mainContact container">
                     <span className="hello">Hello,</span>
-                    <span className="maintext">I'm <span className="names">John</span> <br /> Frontend Developer </span>
+                    <span className="maintext">
+                        I'm <span className="names">
+                            <ReactTyped
+                                strings={["Raxmonov Ulug'bek"]}
+                                typeSpeed={40}
+                                startDelay={500}
+                                showCursor={false}
+                            />
+                        </span> 
+                        <br /> 
+                        <ReactTyped
+                            strings={["Frontend Developer"]}
+                            typeSpeed={40}
+                            startDelay={2000}
+                            showCursor={false}
+                        />
+                    </span>
                     <p className="par">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br />
-Deserunt voluptas aut accusamus ?
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br />
+                        Deserunt voluptas aut accusamus?
                     </p>
-                    <Link to='home'><button className="btn"><PiHandbagSimpleFill  className='btnImg' /> Hiro Me</button></Link>
+                    <Link to='home' smooth={true} duration={500}>
+                        <button className="btn">
+                            <PiHandbagSimpleFill className='btnImg' />
+                            Hire Me
+                        </button>
+                    </Link>
                 </div>
-                <img src={rasm} alt="" className="bg" />
-             </section>
+                <img src={rasm} alt="Background" className="bg" />
+            </section>
         </div>
     );
 }
